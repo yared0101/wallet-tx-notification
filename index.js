89f305d53dce1435dbca67199f03b1e995bc18dc
Yared Terefe <yaredterefeg@gmail.com>
2022-10-27 15:31:26 +0300

    express added into the mix

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+const express = require("express");
 const { defaultTime, bot, prisma } = require("./config");
 const { intervalFunction, processPending } = require("./services");
 const { subscribe } = require("./utils/cryptoFunctions");
+const app = express();
+app.use(await bot.createWebhook({ domain: process.env.BOT_DOMAIN }));
+
 const main = async () => {
     const configData = await prisma.time.findFirst();
     const setTime = configData?.totalTime;
@@ -13,10 +17,7 @@ require("./src/commands")(bot);
 require("./src/actions")(bot);
 require("./src/hears")(bot);
 require("./src/raw_inputs")(bot);
-// bot.launch({
-//     webhook: {
-//         domain: 'http://159.223.228.24',
-//         port: 8080
-//     }});
-module.exports = bot;
+
+app.listen(8080, () => console.log("Listening on port", 8080));
+// module.exports = bot;
 console.log("started");
